0c94ba6bd3d41e70f6d15e685871a69b57b19a97
Laura Evans <laura.evans@ivieinc.com>
2016-01-17 17:34:40 -0600

    sort student info results by updatedAt

--- ListViewController.swift
+++ ListViewController.swift
@@ -26,6 +26,7 @@ class ListViewController: UIViewController {
                 
         OTMClient.sharedInstance().loadStudentInformation { (result, errorString) -> Void in
             if let locations = result {
+                self.locations = locations
                 dispatch_async(dispatch_get_main_queue(), {
                     self.listTableView.reloadData()
                 })
--- On The Map/OTMConvinience.swift
+++ On The Map/OTMConvinience.swift
@@ -99,7 +99,7 @@ extension OTMClient {
     
     func loadStudentInformation(completionHandler: (result: [OTMStudentInformation]?, errorString: String?) -> Void)
     {
-        let request = NSMutableURLRequest(URL: NSURL(string: "https://api.parse.com/1/classes/StudentLocation")!)
+        let request = NSMutableURLRequest(URL: NSURL(string: "https://api.parse.com/1/classes/StudentLocation?order=-updatedAt")!)
         request.addValue("QrX47CA9cyuGewLdsL7o5Eb8iug6Em8ye0dnAbIr", forHTTPHeaderField: "X-Parse-Application-Id")
         request.addValue("QuWThTdiRmTux3YaDseUSEpUKo7aBYM737yKd4gY", forHTTPHeaderField: "X-Parse-REST-API-Key")
         let session = NSURLSession.sharedSession()
